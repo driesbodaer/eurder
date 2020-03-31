c6692e6a26dbc31f2caf4a6ee3f94f1cdb158e14
Dries Bodaer <dries.bodaer@cegeka.com>
2020-03-31 17:08:39 +0200

    setup

---
+++ api/src/main/java/soloproj/SoloprojApplication.java
@@ -0,0 +1,13 @@
+package com.eurder.soloproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloprojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoloprojApplication.class, args);
+    }
+
+}
