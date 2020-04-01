c083b4d728db621a4e708d7391ddebf1756ba07c
Dries Bodaer <dries.bodaer@cegeka.com>
2020-04-01 11:49:15 +0200

    skeleton

--- api/src/main/java/com/eurder/api/controllers/CustomerController.java
+++ api/src/main/java/com/eurder/api/controllers/CustomerController.java
@@ -1,9 +1,12 @@
 package com.eurder.api.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "customers")
 public class CustomerController {
+    private final Logger LOGGER = LoggerFactory.getLogger(CustomerController.class);
 }
--- api/src/main/java/com/eurder/api/controllers/OrderController.java
+++ api/src/main/java/com/eurder/api/controllers/OrderController.java
@@ -1,9 +1,12 @@
 package com.eurder.api.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "orders")
 public class OrderController {
+    private final Logger LOGGER = LoggerFactory.getLogger(OrderController.class);
 }
